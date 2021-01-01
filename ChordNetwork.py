717e76b1a8bbd5ae8f27d90fcae70c46c0b6c121
SadafAsad <sadaf98x@gmail.com>
2021-01-01 10:58:50 +0330

    #2 structure, addToNetwork added

--- ChordNetwork.py
+++ ChordNetwork.py
@@ -1,28 +1,51 @@
-class Process:
-    def __init__(self, network):
-        self.id = self.__setID(network)
-        self.successor = self.setSuccessor(network)
-        self.predecessor = self.setPredecessor(network)
-        self.FT = self.setFT(network)
+import random
 
-    def __setID(self, network):
-        return 0
-
-    def setSuccessor(self, network):
-        return 0
-
-    def setPredecessor(self, network):
-        return 0
-
-    def setFT(self, network):
-        return 0
+class Agent:
+    def __init__(self):
+        self.id = None
+        self.successor = None
+        self.predecessor = None
+        self.datas = list()
+        self.FT = list()
 
 class Network:
     def __init__(self):
         self.nodes = list()
     
-    def addToNetwork(self, id):
-        pass
+    def addToNetwork(self, agent):
+        # inja lock mishe
+        # id random behesh midam
+        new_id = random.randint(1, 5000)
+        i = 0
+        while i < len(self.nodes):
+            if self.nodes[i].id == new_id:
+                new_id = random.randint(1, 5000)
+                i = 0
+            else:
+                i+=1
+        agent.id = new_id
+        # be shabake ezaf mikonm
+        self.nodes.append(agent)
+        self.nodes.sort(key=lambda agent: agent.id)
+
+        agent_index = self.nodes.index(agent)
+        # successoresh ro moshakhas mikonm
+        self.nodes[agent_index].successor = self.nodes[(agent_index+1)%(len(self.nodes))]
+        # predecessore successoresh ro taghir midam
+        self.nodes[(agent_index+1)%(len(self.nodes))].predecessor = self.nodes[agent_index]
+        # predecessoresh ro moshakhas mikonm
+        self.nodes[agent_index].predecessor = self.nodes[agent_index-1]
+        # successore predecessoresh ro taghir midam
+        self.nodes[agent_index-1].successor = self.nodes[agent_index]
+
+        # data behesh midam
+        for data in self.nodes[agent_index].successor.datas:
+            if data.key <= self.nodes[agent_index].id:
+                self.nodes[agent_index].datas.append(data)
+                self.nodes[agent_index].successor.datas.remove(data)
+
+        # ft update beshe
+        # lock ta inja
 
     def removeFromNetwork(self, id):
         pass
@@ -37,3 +60,6 @@ class Data:
 
     def __setKey(self, data_keys):
         return 0
+
+    def addData(self):
+        pass
